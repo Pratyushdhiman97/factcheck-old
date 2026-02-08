25f15018a79e9c526548c38c658d48f8bb293cce
SIDDHARTH UMAJWAL <160970705+Sid80008@users.noreply.github.com>
2026-02-08 13:08:43 +0530

    Add files via upload

---
+++ factcheck/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero-title glow">News Verification 2026</h1>
+      <p className="hero-subtitle glow">
+        Instantly check authenticity, trust score, and sources of any news
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
